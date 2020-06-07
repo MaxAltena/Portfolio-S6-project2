96f787fca37b62ee6e4e36eea4991b0233ccdd04
Max Altena <Max@MaxAltena.com>
2020-06-08 00:54:57 +0200

    :zap: Add content

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -68,7 +68,7 @@
 	<h2>Conclusie</h2>
 	<p></p>
 	<h2>Verantwoording</h2>
-	<p></p>
+	<p>Volledige uitgevoerd door mijzelf.</p>
 {:else if product.link === 'projectplan'}
 	<h2>Aanleiding</h2>
 	<p></p>
@@ -79,51 +79,62 @@
 	<h2>Conclusie</h2>
 	<p></p>
 	<h2>Verantwoording</h2>
-	<p></p>
+	<p>Gemaakt samen met Dylano Hartman en Mark Hendriks. Ontwerp van document en uiteindelijke oplevering gedaan door mijzelf.</p>
 {:else if product.link === 'concurrentie-analyse'}
 	<h2>Aanleiding</h2>
-	<p></p>
+	<p>Om te weten te komen wat het uiteindelijke component in het WijPIDZ product voor het delen van kennis gaat inhouden en wat voor vorm deze zal hebben zal een concurrentie analyse worden gedaan. Met deze analyse wordt gekeken naar welke verschillende vormen er zijn, wat de voor- en nadelen hiervan zijn en waar rekening mee moet worden gehouden tijdens het bedenken en ontwerpen van het component. Hieruit zullen uiteindelijk mogelijke USP’s komen, de sterktes en zwaktes van de concurrenten en dus ook de kansen en gevaren van het component.</p>
 	<h2>Aanpak</h2>
-	<p></p>
+	<p>Het eerste dat er gedaan moet worden om concurrenten te kunnen analyseren is het vinden van concurrenten op basis van het product of service dat wordt geanalyseerd. Dit wordt gedaan nadat het product of service dat moet worden vergeleken is vastgesteld. Hierbij wordt dan gekeken naar diverse punten, denk hier bijvoorbeeld aan prijs, kwaliteit en service. Het analyseren van de concurrenten is het belangrijkste en meeste werk, zo moet er per concurrent gekeken worden naar de opgestelde punten en hoe ze hierop scoren ten opzichte van de andere. Per punt wordt er ook een kleine toelichting gegeven om zo de score van het punt te onderbouwen. Als laatste kan er dan bestudeerd worden welke sterke en zwakke punten elke concurrent heeft. Hiermee kunnen mogelijke USP’s verschijnen die het product of service uniek kunnen maken waarbij waarde kan worden gecreëerd.</p>
 	<h2>Uitvoering</h2>
 	<p></p>
 	<h2>Conclusie</h2>
-	<p></p>
+	<p>Er is geen directe concurrent met een platform waar er gemakkelijk kennis kan worden gedeeld en waar gebruikers gemakkelijk met elkaar in contact kunnen komen. Verder is er wel indirecte concurrentie van bestaande platformen, zoals Reddit en StackExchange, die de beoogde functionaliteiten van het product hebben. Als het product uiteindelijk soortgelijke functionaliteiten heeft als de indirecte concurrenten kan het op maat gemaakt worden voor de behoeftes van de gebruikers. Er zal dus wel gekeken kunnen worden naar hoe deze platformen deze functionaliteiten hebben verwerkt om inspiratie op te kunnen doen.</p>
 	<h2>Verantwoording</h2>
-	<p></p>
+	<p>Volledige uitgevoerd door mijzelf.</p>
 {:else if product.link === 'trend-analyse'}
 	<h2>Aanleiding</h2>
-	<p></p>
+	<p>Deze analyse wordt gedaan om verschillende trends te signaleren en te analyseren. Met de analyses van deze trends kan het concept worden vastgesteld waar er op dit moment vraag naar is.</p>
 	<h2>Aanpak</h2>
-	<p></p>
+	<p>Eerst zal er gekeken worden naar relevante trends in de zorg. Sommige trends zijn belangrijker en relevanter voor het concept. Bij elke trend wordt wat verteld over wat voor soort trend het is en hoe deze de markt verandert en/of wat het voor de markt betekent. Daarnaast wordt er gekeken naar de relevantie voor het product en de mogelijke kansen. Er wordt een enkele trend gekozen die het meest relevante is voor het product. Met deze trend zal er een consumer trend canvas gemaakt worden om alle informatie gemakkelijk weer te geven en inzichtelijk te maken.</p>
 	<h2>Uitvoering</h2>
 	<p></p>
 	<h2>Conclusie</h2>
-	<p></p>
+	<p>Het delen van kennis binnen de zorg zal de komende tijd belangrijker worden. Mede door de huidige COVID-19 crisis is het directe contact tussen een zorgverlener en zorgontvanger niet aanwezig en gaat dit geheel digitaal. Deze trend van het digitaal gaan en het delen van kennis via andere wegen dan de traditionele kan op worden ingespeeld. Deze innovatiepotentie zal in het WijPIDZ component terugkomen om een aantrekkelijker product te maken voor de gebruiker.</p>
 	<h2>Verantwoording</h2>
+	<p>Volledige uitgevoerd door mijzelf.</p>
+{:else if product.link === 'concept-document'}
+	<h2>Aanleiding</h2>
+	<p>Dit document is gemaakt om het snel gemaakte proof of concept te verduidelijken voor de opdrachtgever.</p>
+	<h2>Aanpak</h2>
+	<p>Er wordt een proof of concept gemaakt om visueel weer te geven wat de ideeën zijn voor het concept. Hierbij worden de functionaliteiten van het platform uitgelicht.  Naast het concept te visualiseren en te onderbouwen zijn er Unique Selling Points om het concept te versterken en te verantwoorden.</p>
+	<h2>Uitvoering</h2>
 	<p></p>
+	<h2>Conclusie</h2>
+	<p>Het concept wordt nog getoetst bij de opdrachtgever maar is nu al erg belangrijk door het gemakkelijk te visualiseren van het idee. Dit helpt andere met het begrijpen van het concept en functionaliteiten van het concept. Na feedback van de opdrachtgever zal deze mogelijk worden verwerkt in de volgende fases van het project.</p>
+	<h2>Verantwoording</h2>
+	<p>Volledige uitgevoerd door mijzelf.</p>
 {:else if product.link === 'concept-analyse'}
 	<h2>Aanleiding</h2>
-	<p></p>
+	<p>Om het concept te versterken en beter te kunnen positioneren in de markt wordt er een concept analyse gedaan, bestaande uit een SWOT en USP analyse. Met deze informatie kan worden verantwoord welke functionaliteiten er in het concept moeten zitten en welke tot een USP kunnen leiden.</p>
 	<h2>Aanpak</h2>
-	<p></p>
+	<p>Er wordt eerst een SWOT analyse gedaan van het concept, wat zijn punten die sterk zijn van het concept en welke zijn de zwaktes? Met deze vragen beantwoord te hebben kan er nog gekeken worden naar de concurrentie om zo het concept in de markt te kunnen zetten met een Unique Selling Point.</p>
 	<h2>Uitvoering</h2>
 	<p></p>
 	<h2>Conclusie</h2>
-	<p></p>
+	<p>Doordat het concept de problemen van de gebruiker oplost is dat sterke punt ook een van de unieke punten van het concept. Naast dat het concept een probleem van de gebruiker oplost is het ook voor de gebruiker ontworpen en gemaakt. Verder is het aanbieden van een platform zo groot als WijPIDZ met functionaliteiten die nuttig zijn voor de gebruiker een USP op zich. Het delen van kennis component in het WijPIDZ platform is op zichzelf een uniek concept om iedereen zijn/haar kennis te delen door vragen te beantwoorden of kennis op te doen door het vragen van vragen. Daarnaast is het verifiëren van antwoorden door de community een USP.</p>
 	<h2>Verantwoording</h2>
-	<p></p>
+	<p>Volledige uitgevoerd door mijzelf.</p>
 {:else if product.link === 'business-model-canvas'}
 	<h2>Aanleiding</h2>
-	<p></p>
+	<p>Om de opdrachtgever en andere belanghebbende van het concept en het uiteindelijke product duidelijk te maken wat de business erachter is wordt er een Business Model Canvas gemaakt. Op deze manier kan gemakkelijk en in een oogopslag duidelijk zijn wat de waarde is.</p>
 	<h2>Aanpak</h2>
-	<p></p>
+	<p>Er zal worden gekeken naar de manieren waarop het product geld kan maken, op wat voor manieren er geld wordt uitgegeven, waardeproposities en klantgerichte punten. Mede door de workshop gegeven door Bob Nieme, de eigenaar van Data Streams, kan er goed aan de slag worden gegaan met het maken van een Business Model Canvas om al deze punten op een overzichtelijke manier weer te geven.</p>
 	<h2>Uitvoering</h2>
 	<p></p>
 	<h2>Conclusie</h2>
-	<p></p>
+	<p>Duidelijkheid van welke belanghebbende en kosten er zijn met dit product is er nu. Daarnaast was er nog niet over nagedacht hoe het product waarde creëert voor de verschillende klanten en belanghebbende. Zo is het voor PIDZ belangrijk dat het product er is om meer gebruikers aan te trekken voor het platform en deze te behouden. De ZZP’ers in de zorg hebben verschillende vragen en door het product wordt het gemakkelijker om deze vragen te stellen en te beantwoorden op een overzichtelijke manier.</p>
 	<h2>Verantwoording</h2>
-	<p></p>
+	<p>Volledige uitgevoerd door mijzelf.</p>
 {:else}
 	<p>Huh, product niet gevonden? Probeer het later nog eens of check of dit wel de goede link is!</p>
 {/if}
